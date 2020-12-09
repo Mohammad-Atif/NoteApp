93bd1136be040ff5ffc7c619aa25dc293a8d24a5
atif9 <mohammadatif92080@gmail.com>
2020-12-09 15:53:27 +0530

    recyclerview success

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -4,18 +4,53 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.noteapp.dbqueries.Notes
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var noteadapter:NoteAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Handler().postDelayed(
-            {
-                val intent = Intent(this, noteadding::class.java)
-                startActivity(intent)
-            },2000
-        )
+//        Handler().postDelayed(
+//            {
+//                val intent = Intent(this, noteadding::class.java)
+//                startActivity(intent)
+//            },2000
+//        )
+        initrecyclerview()
+        adddata()
 
     }
+
+    private fun initrecyclerview()
+    {
+        recyclerviewmain.apply {
+            layoutManager=LinearLayoutManager(this@MainActivity)
+            noteadapter= NoteAdapter()
+            adapter=noteadapter
+        }
+    }
+
+    private fun createdummydatas():List<Notes>
+    {
+        val notesdata=ArrayList<Notes>()
+        notesdata.add(Notes("Meeting","Hey i have a very important meeting tommorow"))
+        notesdata.add(Notes("test","Hey this is for test blogging can be seen as a form of social networking service. Indeed, bloggers not only produce content to post on their blogs but also often build social relations with their reade"))
+        notesdata.add(Notes("dummy","this is a dummy data"))
+        notesdata.add(Notes("dmmy","this isdfsfgdummy data"))
+        notesdata.add(Notes("dmdmy","this isdfsfg sdfgdummy data"))
+        notesdata.add(Notes("Recycler view","A blog (a truncation of \"weblog\")[1] is a discussion or informational website published on the World Wide Web consisting of discrete, often informal diary-style text entries (posts). Posts are typically displayed in reverse chronological order, so that the most recent post appears first, at the top of the web page. Until 2009, blogs were usually the work of a single individual,[citation needed] occasionally of a small group, and often covered a single subject or topic."))
+        notesdata.add(Notes("Recyfw","hey this isadfdf dof  test for recycler view"))
+        return notesdata
+    }
+
+    private fun adddata()
+    {
+        val data=createdummydatas()
+        noteadapter.submitList(data)
+    }
+
 }
---
+++ app/src/main/java/com/example/noteapp/NoteAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.noteapp
+
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.noteapp.dbqueries.Notes
+import kotlinx.android.synthetic.main.layout_note_card.view.*
+import java.util.ArrayList
+
+class NoteAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    private var noteslist: List< Notes > = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return notesviewholder(
+            LayoutInflater.from(parent.context).inflate(R.layout.layout_note_card,parent,false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder) {
+
+            is notesviewholder -> {
+                holder.bind(noteslist.get(position))
+            }
+
+        }
+    }
+
+
+    override fun getItemCount(): Int {
+        return noteslist.size
+    }
+
+    fun submitList(notlists: List<Notes>){
+        noteslist = notlists
+    }
+
+    class notesviewholder constructor(itemview : View) :RecyclerView.ViewHolder(itemview){
+        val notestitle=itemview.Titletextview
+        val notestext=itemview.notetxtview
+        val checkboxes=itemview.notecheckbox
+        val doneview=itemview.donetxtview
+        val rembtn=itemview.removenotebtn
+
+        fun bind(note:Notes)
+        {
+            notestitle.setText(note.title)
+            notestext.setText(note.Note)
+
+        }
+    }
+
+}
